9021ce7df8f3b3c92c01144d538fdff2b8105cd3
artemponomarevjetski <52363559+artemponomarevjetski@users.noreply.github.com>
2020-04-10 11:27:30 -0700

    Update prop_nested_str.py

--- prop_nested_str.py
+++ prop_nested_str.py
@@ -9,6 +9,7 @@ Created on Sun Feb  9 19:05:40 2020
 def solution(S):
     # write your code in Python 3.6
     """
+    Task description is given on Codility website, https://app.codility.com/programmers/
     test for properly nested ( and )
     """
     N = len(S)
