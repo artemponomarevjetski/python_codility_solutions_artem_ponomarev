607df4a36b02c9f3273252327f5376e60853bd2e
artemponomarevjetski <52363559+artemponomarevjetski@users.noreply.github.com>
2020-04-10 09:46:59 -0700

    Update equileader.py

--- equileader.py
+++ equileader.py
@@ -9,6 +9,7 @@ Created on Sun Feb 16 14:02:56 2020
 def solution(A):
     # write your code in Python 3.6
     """
+    Task description is given on Codility website, https://app.codility.com/programmers/
     equileader
     """
     N = len(A)
