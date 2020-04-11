3997ff0469e188ad142733925de7aea5a99db198
Artem Ponomarev <52363559+artemponomarevjetski@users.noreply.github.com>
2020-04-11 14:27:43 -0700

    Update max_double_slice.py

--- max_double_slice.py
+++ max_double_slice.py
@@ -12,6 +12,7 @@ Created on Fri Apr 10 09:18:46 2020
 def solution(A):
     # write your code in Python 3.6
     """
+    Task description is given on Codility website, https://app.codility.com/programmers/
     this is the solution for the max double slice problem
     """
     N = len(A)
