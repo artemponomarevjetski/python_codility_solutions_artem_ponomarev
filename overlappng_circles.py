afc19b9a7eb3aa38d28db8a0e9e27ccf8e12654e
Artem Ponomarev <52363559+artemponomarevjetski@users.noreply.github.com>
2020-05-04 08:37:20 -0700

    Update overlappng_circles.py

--- overlappng_circles.py
+++ overlappng_circles.py
@@ -42,6 +42,36 @@ def solution(A):
 #A = [1, 1, 1]
 print("result  = ", solution([1, 5, 2, 1, 4, 0]))
 #
+#Analysis
+#Detected time complexity:
+#O(N*log(N)) or O(N)
+#expand allExample tests
+#▶ example1
+#example test ✔OK
+#expand allCorrectness tests
+#▶ simple1 ✔OK
+#▶ simple2 ✔OK
+#▶ simple3 ✔OK
+#▶ extreme_small
+#empty and [10] ✔OK
+#▶ small1 ✔OK
+#▶ small2 ✔OK
+#▶ small3 ✔OK
+#▶ overflow
+#arithmetic overflow tests ✔OK
+#expand allPerformance tests
+#▶ medium1 ✔OK
+#▶ medium2 ✔OK
+#▶ medium3 ✔OK
+#▶ medium4 ✔OK
+#▶ 10M_intersections
+#10.000.000 intersections ✔OK
+#▶ big1 ✔OK
+#▶ big2 ✔OK
+#▶ big3
+#[0]*100.000 ✔OK
+
+#
 #Task description
 #We draw N discs on a plane. The discs are numbered from 0 to N − 1. An array A of N non-negative integers, specifying the radiuses of the discs, is given. The J-th disc is drawn with its center at (J, 0) and radius A[J].
 #
@@ -74,33 +104,3 @@ print("result  = ", solution([1, 5, 2, 1, 4, 0]))
 #N is an integer within the range [0..100,000];
 #each element of array A is an integer within the range [0..2,147,483,647].
 #Copyright 2009–2020 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
-#
-#
-#Analysis
-#Detected time complexity:
-#O(N*log(N)) or O(N)
-#expand allExample tests
-#▶ example1
-#example test ✔OK
-#expand allCorrectness tests
-#▶ simple1 ✔OK
-#▶ simple2 ✔OK
-#▶ simple3 ✔OK
-#▶ extreme_small
-#empty and [10] ✔OK
-#▶ small1 ✔OK
-#▶ small2 ✔OK
-#▶ small3 ✔OK
-#▶ overflow
-#arithmetic overflow tests ✔OK
-#expand allPerformance tests
-#▶ medium1 ✔OK
-#▶ medium2 ✔OK
-#▶ medium3 ✔OK
-#▶ medium4 ✔OK
-#▶ 10M_intersections
-#10.000.000 intersections ✔OK
-#▶ big1 ✔OK
-#▶ big2 ✔OK
-#▶ big3
-#[0]*100.000 ✔OK
