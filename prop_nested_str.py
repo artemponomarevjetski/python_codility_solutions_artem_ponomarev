8a9b20285e9ac2d4b5eaef6a10ba1e06db130460
Artem Ponomarev <52363559+artemponomarevjetski@users.noreply.github.com>
2020-05-04 08:41:06 -0700

    Update prop_nested_str.py

--- prop_nested_str.py
+++ prop_nested_str.py
@@ -39,28 +39,6 @@ def solution(S):
 
 # main()
 print("result  = ", solution("(()(())())"))
-
-#Task description
-#A string S consisting of N characters is called properly nested if:
-#
-#S is empty;
-#S has the form "(U)" where U is a properly nested string;
-#S has the form "VW" where V and W are properly nested strings.
-#For example, string "(()(())())" is properly nested but string "())" isn't.
-#
-#Write a function:
-#
-#def solution(S)
-#
-#that, given a string S consisting of N characters, returns 1 if string S is properly nested and 0 otherwise.
-#
-#For example, given S = "(()(())())", the function should return 1 and given S = "())", the function should return 0, as explained above.
-#
-#Write an efficient algorithm for the following assumptions:
-#
-#N is an integer within the range [0..1,000,000];
-#string S consists only of the characters "(" and/or ")".
-#Copyright 2009–2020 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 #
 #Analysis summary
 #The solution obtained perfect score.
@@ -90,3 +68,25 @@ print("result  = ", solution("(()(())())"))
 #sequence of full trees of the form T=(TT), depths [1..10..1], with/without unmatched ')' at the end, length=49K+ ✔OK
 #▶ broad_tree_with_deep_paths
 #string of the form (TTT...T) of 300 T's, each T being '(((...)))' nested 200-fold, length=1 million ✔OK
+
+#Task description
+#A string S consisting of N characters is called properly nested if:
+#
+#S is empty;
+#S has the form "(U)" where U is a properly nested string;
+#S has the form "VW" where V and W are properly nested strings.
+#For example, string "(()(())())" is properly nested but string "())" isn't.
+#
+#Write a function:
+#
+#def solution(S)
+#
+#that, given a string S consisting of N characters, returns 1 if string S is properly nested and 0 otherwise.
+#
+#For example, given S = "(()(())())", the function should return 1 and given S = "())", the function should return 0, as explained above.
+#
+#Write an efficient algorithm for the following assumptions:
+#
+#N is an integer within the range [0..1,000,000];
+#string S consists only of the characters "(" and/or ")".
+#Copyright 2009–2020 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
