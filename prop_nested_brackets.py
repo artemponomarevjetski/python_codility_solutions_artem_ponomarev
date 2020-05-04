6104d5a968e9cad3dfb490c29edb0012dae37961
Artem Ponomarev <52363559+artemponomarevjetski@users.noreply.github.com>
2020-05-04 08:39:04 -0700

    Update prop_nested_brackets.py

--- prop_nested_brackets.py
+++ prop_nested_brackets.py
@@ -45,28 +45,6 @@ def solution(S):
 # main()
 print("result  = ", solution('{[()]}'))
 
-#
-#Task description
-#A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
-#
-#S is empty;
-#S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
-#S has the form "VW" where V and W are properly nested strings.
-#For example, the string "{[()()]}" is properly nested but "([)()]" is not.
-#
-#Write a function:
-#
-#def solution(S)
-#
-#that, given a string S consisting of N characters, returns 1 if S is properly nested and 0 otherwise.
-#
-#For example, given S = "{[()()]}", the function should return 1 and given S = "([)()]", the function should return 0, as explained above.
-#
-#Write an efficient algorithm for the following assumptions:
-#
-#N is an integer within the range [0..200,000];
-#string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".
-#Copyright 2009–2020 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 #Analysis
 #Detected time complexity:
 #O(N)
@@ -93,3 +71,26 @@ print("result  = ", solution('{[()]}'))
 #sequence of full trees of the form T=(TT), depths [1..10..1], with/without some brackets at the end, length=49K+ ✔OK
 #▶ broad_tree_with_deep_paths
 #string of the form [TTT...T] of 300 T's, each T being '{{{...}}}' nested 200-fold, length=120K+ ✔OK
+
+#
+#Task description
+#A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
+#
+#S is empty;
+#S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
+#S has the form "VW" where V and W are properly nested strings.
+#For example, the string "{[()()]}" is properly nested but "([)()]" is not.
+#
+#Write a function:
+#
+#def solution(S)
+#
+#that, given a string S consisting of N characters, returns 1 if S is properly nested and 0 otherwise.
+#
+#For example, given S = "{[()()]}", the function should return 1 and given S = "([)()]", the function should return 0, as explained above.
+#
+#Write an efficient algorithm for the following assumptions:
+#
+#N is an integer within the range [0..200,000];
+#string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".
+#Copyright 2009–2020 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
