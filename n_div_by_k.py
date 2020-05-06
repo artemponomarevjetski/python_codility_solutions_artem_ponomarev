17c63ef0a946c4b8a45f5200b601b62f700241a6
Artem Ponomarev <52363559+artemponomarevjetski@users.noreply.github.com>
2020-05-06 09:42:30 -0700

    Update n_div_by_k.py

--- n_div_by_k.py
+++ n_div_by_k.py
@@ -5,24 +5,19 @@ Created on Fri Feb  7 19:42:35 2020
 
 @author: artemponomarev
 """
-A=6
-B=11
-K=2
+
 def solution(A, B, K):
     """"
     Task description is given on Codility website, https://app.codility.com/programmers/
     number of divisibles
     """
-   
-    if A>B or A<0 or B<0 or K<=0 or K>2e9 or B>2e9:
+
+    if A > B or A < 0 or B < 0 or K <= 0 or K > 2e9 or B > 2e9:
         return 0
-   
-   
-    if (A % K == 0): 
+
+    if A % K == 0:
         return (int(B / K) - int(A / K)) + 1
-   
-  
-    return (int(B / K) - int(A / K))
-    
-# main()
-print("result  = ", solution(A, B, K))
+
+    return int(B / K) - int(A / K)
+
+print("result  = ", solution(6, 11, 2))
